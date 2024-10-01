d2f21f77cc401f90b938160b644360291ff9ad98
Thor-B <eaa23thbj@students.eaaa.dk>
2024-10-01 09:33:17 +0200

    ClientThreadStuff

--- src/game/ClientGameLogic.java
+++ src/game/ClientGameLogic.java
@@ -3,15 +3,22 @@ package game;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ClientGameLogic {
     private static DataOutputStream outToServer;
+    private static ArrayList<Player> playerList = new ArrayList<Player>();
+    private static BufferedReader bufferedReader;
+
+    private static ClientListenThread clientListenThread = new ClientListenThread(bufferedReader);
 
     public static void setOutToServer(DataOutputStream outToServer) {
         ClientGameLogic.outToServer = outToServer;
     }
 
-
+    public static void setPlayerList(ArrayList<Player> newPlayerList){
+        playerList = newPlayerList;
+    }
 
     public static void playerMoved(int delta_x, int delta_y, String direction) {
         try {
--- src/game/ClientListenThread.java
+++ src/game/ClientListenThread.java
@@ -1,9 +1,14 @@
 package game;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class ClientListenThread extends Thread{
+public class ClientListenThread extends Thread {
     private final BufferedReader inFromServer;
+    private String stringFromServer;
+    private ArrayList<Player> playerArrayList = new ArrayList<>();
+    ;
 
     public ClientListenThread(BufferedReader infromServer) {
         this.inFromServer = infromServer;
@@ -13,10 +18,19 @@ public class ClientListenThread extends Thread{
     public void run() {
         try {
             Thread.sleep(3000);
+            stringFromServer = inFromServer.readLine();
+            playerArrayList.clear();
+            String[] playerStrings = stringFromServer.split("#");
+            for (String playerString : playerStrings) {
+                String[] playerInfo = playerString.split(",");
+                pair pair = new pair(Integer.valueOf(playerInfo[1]), Integer.valueOf(playerInfo[2]));
+                Player player = new Player(playerInfo[0], pair, playerInfo[3], Integer.valueOf(playerInfo[4]));
+                playerArrayList.add(player);
+            }
 
+          ClientGameLogic.setPlayerList(playerArrayList);
 
-
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
