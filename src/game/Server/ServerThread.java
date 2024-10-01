0e18819e2d82a36b230c68517be3605cbae94e92
Smeaguuul <mikkelhoffmannp@gmail.com>
2024-10-01 09:36:08 +0200

    Stuff

--- src/game/Server/ServerThread.java
+++ src/game/Server/ServerThread.java
@@ -9,36 +9,40 @@ import static game.Server.ServerGameLogic.updatePlayer;
 
 public class ServerThread extends Thread{
 	Socket connSocket;
+	private Player client;
 
 	public ServerThread(Socket connSocket) {
 		this.connSocket = connSocket;
 	}
 	public void run() {
 		try {
+			//Opretter forbindelserne
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
 			DataOutputStream outToClient = new DataOutputStream(connSocket.getOutputStream());
 			ServerGameLogic.addConnection(outToClient);
 
-			// Do the work and the communication with the client here
-			// The following two lines are only an example
+			//Opretter spilleren
 			String clientNavn = inFromClient.readLine();
-			Player client = ServerGameLogic.makePlayers(clientNavn);
+			client = ServerGameLogic.makePlayers(clientNavn);
 			System.out.println(clientNavn);
 
 			while (true) {
 				String clientMessage = inFromClient.readLine();
-				switch (clientMessage) {
-					case "UP":    updatePlayer(client, 0,-1,"up");   break;
-					case "DOWN":  updatePlayer(client,0,+1,"down");  break;
-					case "LEFT":  updatePlayer(client,-1,0,"left");  break;
-					case "RIGHT": updatePlayer(client,+1,0,"right"); break;
-					default: break;
-				}
+				receiveMove(clientMessage);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// do the work here
+	}
+
+	private void receiveMove(String clientMessage) {
+		switch (clientMessage) {
+			case "UP":    updatePlayer(client, 0,-1,"up");   break;
+			case "DOWN":  updatePlayer(client,0,+1,"down");  break;
+			case "LEFT":  updatePlayer(client,-1,0,"left");  break;
+			case "RIGHT": updatePlayer(client,+1,0,"right"); break;
+			default: break;
+		}
 	}
 
 
